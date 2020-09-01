c030416c37d539aff4c4297b580b76664aa78b13
Michael Rzemien <40279622+marzemien@users.noreply.github.com>
2020-09-01 12:37:34 -0400

    Create run_analysis.R
    Initial upload of run_analysis from person copy of R Studio.

---
+++ run_analysis.R
@@ -0,0 +1,56 @@
+install.packages("tidyverse")
+install.packages("haven")
+library(tidyverse)
+library(haven)
+library(purrr)
+
+URL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(URL, destfile = "ziped")
+unzip(paste0(getwd(), "/", "ziped"))
+
+TestList <- list.files(paste0(getwd(), "/UCI HAR Dataset/test/Inertial Signals"))
+TrainList <- list.files(paste0(getwd(), "/UCI HAR Dataset/train/Inertial Signals"))
+TestFiles <- map_chr(TestList, ~ paste0(getwd(), "/UCI HAR Dataset/test/Inertial Signals/", .x))
+TrainFiles <- map_chr(TrainList, ~ paste0(getwd(), "/UCI HAR Dataset/train/Inertial Signals/", .x))
+
+varnames <- c("x-axis acceleration", "y-axis acceleration y", "z-axis acceleration", 
+              "x-axis angular velocity", "y-axis angular velocity", "z-axis angular velocity", 
+              "total x-axis acceleration", "total y-axis acceleration", "total z-axis acceleration")
+
+#TestFiles
+HoldmatrixTest <- matrix(nrow = 377216, ncol = 9)
+for(i in seq_along(TestFiles)){
+  HoldmatrixTest[ ,i] <- subset(file <- 
+                                  as.character(read.delim(TestFiles[i], header = FALSE)[ ,1]) %>%
+                                  str_split(" ") %>%
+                                  unlist() %>%
+                                  as.numeric(), !is.na(file))
+}
+#TrainFiles
+HoldmatrixTrain <- matrix(nrow = 941056, ncol = 9)
+for(i in seq_along(TrainFiles)){
+  HoldmatrixTrain[ ,i] <- subset(file <- 
+                                   as.character(read.delim(TrainFiles[i], header = FALSE)[ ,1]) %>%
+                                   str_split(" ") %>%
+                                   unlist() %>%
+                                   as.numeric(), !is.na(file))
+}
+
+#Putting it all together
+Full_Data <- bind_rows(as.data.frame(HoldmatrixTest), as.data.frame(HoldmatrixTrain))
+rm(HoldmatrixTrain, HoldmatrixTest)
+rm(file, i)
+colnames(Full_Data) <- varnames
+
+
+#Mean and SD data.frame
+msd <- rbind(map_dbl(Full_Data, mean),
+             map_dbl(Full_Data, sd))
+rownames(msd) <- c("mean", "sd")
+print(msd)
+head(Full_Data)
+
+
+
+
+
