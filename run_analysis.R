7bbfe3b41192f915ecb93f1eac809ca465256650
Michael Rzemien <40279622+marzemien@users.noreply.github.com>
2020-09-01 21:06:54 -0400

    Update run_analysis.R
    Fix a typo

--- run_analysis.R
+++ run_analysis.R
@@ -13,7 +13,7 @@ TrainList <- list.files(paste0(getwd(), "/UCI HAR Dataset/train/Inertial Signals
 TestFiles <- map_chr(TestList, ~ paste0(getwd(), "/UCI HAR Dataset/test/Inertial Signals/", .x))
 TrainFiles <- map_chr(TrainList, ~ paste0(getwd(), "/UCI HAR Dataset/train/Inertial Signals/", .x))
 
-varnames <- c("x-axis acceleration", "y-axis acceleration y", "z-axis acceleration", 
+varnames <- c("x-axis acceleration", "y-axis acceleration", "z-axis acceleration", 
               "x-axis angular velocity", "y-axis angular velocity", "z-axis angular velocity", 
               "total x-axis acceleration", "total y-axis acceleration", "total z-axis acceleration")
 
